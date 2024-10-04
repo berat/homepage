2a3be90a89a42cc2c8588d16049a9dcc1b9d25ec
Berat Bozkurt <beratbozkurt1999@gmail.com>
2024-10-04 15:13:55 +0300

    change optimize images for now set true

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    unoptimized: false,
+    unoptimized: true,
     domains: [
       "images.unsplash.com",
       "beratbozkurt.net",
