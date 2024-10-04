1b858d12154aaf97173741572cb65bea60852cdc
Berat Bozkurt <beratbozkurt1999@gmail.com>
2024-10-04 11:29:46 +0300

    fixed tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
