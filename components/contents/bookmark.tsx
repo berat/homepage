55336585ac0bad391099b14ca251209f9276cd90
Berat Bozkurt <beratbozkurt1999@gmail.com>
2024-11-21 11:19:03 +0300

    update nextjs to 15 and fix key issue

--- components/contents/bookmark.tsx
+++ components/contents/bookmark.tsx
@@ -13,7 +13,7 @@ const BookmarkContent = async () => {
       {Object.keys(data).map((key: string) => (
         <ul key={key} className="w-full my-4 block">
           <h3 className="text-xl mb-2 text-slate-400 tracking-wide">{key}</h3>
-          <div className={"flex gap-8 flex-wrap"}>
+          <div className={"flex gap-8 flex-wrap"} key={key} >
             {data[key].map((item: BookmarkType) => (
               <BookmarkCard key={item.id} data={item} />
             ))}
