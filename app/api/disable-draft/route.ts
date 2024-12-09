1113ce880209320dead2a4e6b0bc6b2f1e61e89d
Berat Bozkurt <beratbozkurt1999@gmail.com>
2024-12-09 12:20:54 +0300

    fix: disabled on disableddraft api

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,6 +1,6 @@
 import { draftMode } from "next/headers";
 
 export async function GET() {
-  (await draftMode()).enable();
+  (await draftMode()).disable();
   return new Response("Draft mode is disabled");
 }
