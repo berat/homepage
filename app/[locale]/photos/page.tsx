096f5e671fc1ea5d606060ac62f8729d3d82cd30
Berat Bozkurt <beratbozkurt1999@gmail.com>
2026-01-25 03:06:15 +0300

    added views for photos

--- app/[locale]/photos/page.tsx
+++ app/[locale]/photos/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { getPhotos } from "@/lib/unsplash";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { Key } from "react";
+import { getViewAndLike } from "@/lib/redis/views";
 
 export async function generateMetadata({
   params,
@@ -36,12 +37,29 @@ export default async function Home({
 
   const texts = messages[locale];
 
+  let views = 0;
+
+  try {
+    const { data } = await getViewAndLike("page", "page:" +(locale === "tr" ? "" : "en/") + "photos");
+
+    views = (data?.views ?? 0) + 1;
+  } catch (e) {
+    // build’i kırma
+    views = 0;
+  }
+
   return (
     <div
       id="photos"
       className="max-w-[85%] md:max-w-5xl mx-auto my-16 flex flex-col gap-10"
     >
-      <h1 className="text-4xl text-primary font-bold">{texts.photos}</h1>
+
+      <header className="flex flex-col gap-2.5">
+        <small className="text-gray text-sm font-medium">
+          {Number(views).toLocaleString("tr-TR")} {texts.totalViews}
+        </small>
+        <h1 className="text-4xl text-primary font-bold">{texts.photos}</h1>
+      </header>
       <div className="columns-1 sm:columns-3 lg:columns-4 gap-4">
         {photoData.map(
           (data: {
