60d6b2d84d55993d9668ef00b73dc6f42948f53e
Berat Bozkurt <beratbozkurt1999@gmail.com>
2026-01-28 12:04:37 +0300

    fixed social media links

--- constants/social.ts
+++ constants/social.ts
@@ -1,5 +1,5 @@
 
 export const TWITTER = "https://twitter.com/beratbuilds";
 export const GITHUB = "https://github.com/berat";
-export const LINKEDIN = "https://www.instagram.com/beratbozkurt0/";
-export const INSTAGRAM = "https://www.linkedin.com/in/beratbozkurt/";
+export const LINKEDIN = "https://www.linkedin.com/in/beratbozkurt/";
+export const INSTAGRAM = "https://www.instagram.com/beratbozkurt0/";
